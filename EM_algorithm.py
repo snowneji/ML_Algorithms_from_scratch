d983440b940f0d237839cbac5c672ec02646b1d5
snowneji <snowneji@hotmail.com>
2017-11-30 00:34:06 -0600

    add EM

---
+++ EM_algorithm.py
@@ -0,0 +1,54 @@
+# For the E-step, with each sample we have
+# For the M-step, we need to find the value of θθ that maximises the QQ function
+
+xs = np.array([(5,5), (9,1), (8,2), (4,6), (7,3)])
+thetas = np.array([[0.6, 0.4], [0.5, 0.5]])
+
+tol = 0.01
+max_iter = 100
+
+ll_old = 0
+for i in range(max_iter):
+    ws_A = []
+    ws_B = []
+
+    vs_A = []
+    vs_B = []
+
+    ll_new = 0
+
+    # E-step: calculate probability distributions over possible completions
+    for x in xs:
+
+        # multinomial (binomial) log likelihood
+        ll_A = np.sum([x*np.log(thetas[0])])
+        ll_B = np.sum([x*np.log(thetas[1])])
+
+        # [EQN 1]
+        denom = np.exp(ll_A) + np.exp(ll_B)
+        w_A = np.exp(ll_A)/denom
+        w_B = np.exp(ll_B)/denom
+
+        ws_A.append(w_A)
+        ws_B.append(w_B)
+
+        # used for calculating theta
+        vs_A.append(np.dot(w_A, x))
+        vs_B.append(np.dot(w_B, x))
+
+        # update complete log likelihood
+        ll_new += w_A * ll_A + w_B * ll_B
+
+    # M-step: update values for parameters given current distribution
+    # [EQN 2]
+    thetas[0] = np.sum(vs_A, 0)/np.sum(vs_A)
+    thetas[1] = np.sum(vs_B, 0)/np.sum(vs_B)
+    # print distribution of z for each x and current parameter estimate
+
+    print "Iteration: %d" % (i+1)
+    print "theta_A = %.2f, theta_B = %.2f, ll = %.2f" % (thetas[0,0], thetas[1,0], ll_new)
+
+    if np.abs(ll_new - ll_old) < tol:
+        break
+    ll_old = ll_new
+
